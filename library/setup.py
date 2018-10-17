3ce4b28df77e5d28efce2b6b9a0c26b81192595b
Phil Howard <phil@gadgetoid.com>
2018-10-17 12:37:54 +0100

    Prep for v0.0.2

--- library/setup.py
+++ library/setup.py
@@ -39,7 +39,7 @@ classifiers = ['Development Status :: 4 - Beta',
 
 setup(
     name='ltr559',
-    version='0.0.1',
+    version='0.0.2',
     author='Philip Howard',
     author_email='phil@pimoroni.com',
     description="""Python library for the LTR559 light and proximity""",
